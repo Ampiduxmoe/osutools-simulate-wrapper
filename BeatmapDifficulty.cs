35196285e44f6900024ee1b6b45ba53f1bb7608f
ampiduxmoe <ampiduxmoe@gmail.com>
2024-05-03 12:39:54 +0500

    add support for custom ht rates, implement DA/HR/EZ mod emulation in all endpoints

--- BeatmapDifficulty.cs
+++ BeatmapDifficulty.cs
@@ -15,6 +15,25 @@ public static class BeatmapDifficulty {
         decimal newOd = MsToOverallDifficulty(newHitWindowMs);
         return newOd;
     }
+
+    // https://osu.ppy.sh/wiki/en/Beatmap/Approach_rate#mod-effects
+    public static decimal CalculateHardRockModAR(decimal ar) => Math.Min(ar * 1.4M, 10M);
+    // https://osu.ppy.sh/wiki/en/Beatmap/Circle_size#mod-effects
+    public static decimal CalculateHardRockModCS(decimal cs) => Math.Min(cs * 1.3M, 10M);
+    // https://osu.ppy.sh/wiki/en/Beatmap/Overall_difficulty#mod-effects
+    public static decimal CalculateHardRockModOD(decimal od) => Math.Min(od * 1.4M, 10M);
+    // https://osu.ppy.sh/wiki/en/Beatmap/HP_drain_rate#mod-effects
+    public static decimal CalculateHardRockModHP(decimal hp) => Math.Min(hp * 1.4M, 10M);
+
+    // https://osu.ppy.sh/wiki/en/Beatmap/Approach_rate#mod-effects
+    public static decimal CalculateEasyModAR(decimal ar) => ar / 2M;
+    // https://osu.ppy.sh/wiki/en/Beatmap/Circle_size#mod-effects
+    public static decimal CalculateEasyModCS(decimal cs) => cs / 2M;
+    // https://osu.ppy.sh/wiki/en/Beatmap/Overall_difficulty#mod-effects
+    public static decimal CalculateEasyModOD(decimal od) => od / 2M;
+    // https://osu.ppy.sh/wiki/en/Beatmap/HP_drain_rate#mod-effects
+    public static decimal CalculateEasyModHP(decimal hp) => hp / 2M;
+
     private static decimal ApproachRateToMs(decimal approachRate)
     {
         if (approachRate <= 5)
--- Program.cs
+++ Program.cs
@@ -35,6 +35,40 @@ static List<string> GeneratePerformanceCalculatorArgs(SimulateRequest request, s
     return simulateArgs;  
 }
 
+static List<string> GeneratePerformanceCalculatorArgsDTHT(SimulateRequest request, string? filePath = null)
+{
+    var simulateArgs = new List<string>
+    {
+        "simulate", "osu"
+    };
+    foreach (var mod in request.mods)
+    {
+        string[] modsToSkip = ["da", "hr", "ez"];
+        if (modsToSkip.Any(m => m == mod.ToLower()))
+        {
+            continue;
+        }
+        AddNamedArg(simulateArgs, "mod", mod);
+    }
+    if (request.combo != null)
+    {
+        AddNamedArg(simulateArgs, "combo", request.combo);
+    }
+    AddNamedArg(simulateArgs, "misses", request.misses);
+    AddNamedArg(simulateArgs, "mehs", request.mehs);
+    AddNamedArg(simulateArgs, "goods", request.goods);
+    simulateArgs.Add("--json");
+    if (filePath != null)
+    {
+        simulateArgs.Add(filePath); 
+    }
+    else
+    {
+        simulateArgs.Add(request.beatmap_id.ToString()); 
+    } 
+    return simulateArgs;  
+}
+
 static string GetSimulationJson(List<string> simulationArgs, object simulationLock)
 {
     lock (simulationLock)
@@ -53,12 +87,86 @@ static string GetSimulationJson(List<string> simulationArgs, object simulationLo
     }
 }
 
-static void ChangeBeatmapSpeed(Beatmap map, decimal speedMultiplier) {
+static string GetBeatmapFilePath(int id) => $"cache/{id}.osu";
+static string GetTmpBeatmapFilePath(int id) => "tmp.osu";
+
+static async Task DownloadBeatmapIfNeeded(int id, HttpClient httpClient)
+{
+    var beatmapFilePath = GetBeatmapFilePath(id);
+    if (!File.Exists(beatmapFilePath))
+    {
+        var beatmapFile = await httpClient.GetByteArrayAsync($"https://osu.ppy.sh/osu/{id}");
+        File.WriteAllBytes(beatmapFilePath, beatmapFile);
+    }
+}
+
+static Beatmap GetTmpBeatmap(int id)
+{
+    var beatmapFilePath = GetBeatmapFilePath(id);
+    var tmpBeatmapFilePath = GetTmpBeatmapFilePath(id);
+    if (File.Exists(tmpBeatmapFilePath))
+    {
+        File.Delete(tmpBeatmapFilePath);
+    }
+    File.Copy(beatmapFilePath, tmpBeatmapFilePath);
+    return new Beatmap(tmpBeatmapFilePath);
+}
+
+static void ChangeBeatmapSpeed(Beatmap map, decimal speedMultiplier)
+{
     var ar = map.ApproachRate;
     var od = map.OverallDifficulty;
     map.SetRate(speedMultiplier);
     map.ApproachRate = BeatmapDifficulty.CalculateMultipliedAR(ar, speedMultiplier);
     map.OverallDifficulty = BeatmapDifficulty.CalculateMultipliedOD(od, speedMultiplier);
+}
+
+static void ApplyHR(Beatmap map)
+{
+    map.ApproachRate = BeatmapDifficulty.CalculateHardRockModAR(map.ApproachRate);
+    map.CircleSize = BeatmapDifficulty.CalculateHardRockModCS(map.CircleSize);
+    map.OverallDifficulty = BeatmapDifficulty.CalculateHardRockModOD(map.OverallDifficulty);
+    map.HPDrainRate = BeatmapDifficulty.CalculateHardRockModHP(map.HPDrainRate);
+}
+
+static void ApplyEZ(Beatmap map)
+{
+    map.ApproachRate = BeatmapDifficulty.CalculateEasyModAR(map.ApproachRate);
+    map.CircleSize = BeatmapDifficulty.CalculateEasyModCS(map.CircleSize);
+    map.OverallDifficulty = BeatmapDifficulty.CalculateEasyModOD(map.OverallDifficulty);
+    map.HPDrainRate = BeatmapDifficulty.CalculateEasyModHP(map.HPDrainRate);
+}
+
+static void ApplyDA(Beatmap map, DifficultyAdjustSettings da)
+{
+    map.ApproachRate = da.ar;
+    map.CircleSize = da.cs;
+    map.OverallDifficulty = da.od;
+    map.HPDrainRate = da.hp;
+}
+
+static bool ApplyMods(Beatmap map, IEnumerable<string> mods, DifficultyAdjustSettings? da_settings)
+{
+    var beatmapChanged = false;
+    if (mods.Any(m => m.ToLower() == "da"))
+    {
+        if (da_settings != null)
+        {
+            ApplyDA(map, da_settings);
+            beatmapChanged = true;
+        }
+    }
+    else if (mods.Any(m => m.ToLower() == "hr"))
+    {
+        ApplyHR(map);
+        beatmapChanged = true;
+    }
+    else if (mods.Any(m => m.ToLower() == "ez"))
+    {
+        ApplyEZ(map);
+        beatmapChanged = true;
+    }
+    return beatmapChanged;
 }
 
 var builder = WebApplication.CreateBuilder(args);
@@ -69,11 +177,29 @@ object simulationLock = new();
 object mapEditingLock = new();
 var httpClient = new HttpClient();
 
-app.MapPost("/simulate", (SimulateRequest request) =>
+app.MapPost("/simulate", async (SimulateRequest request) =>
 {
     Console.WriteLine($"POST: /simulate ({request})");
-    var args = GeneratePerformanceCalculatorArgs(request);
-    string response = GetSimulationJson(args, simulationLock);
+    var beatmapId = request.beatmap_id;
+    await DownloadBeatmapIfNeeded(beatmapId, httpClient);
+    string response;
+    lock (mapEditingLock)
+    {
+        var tmpBeatmap = GetTmpBeatmap(beatmapId);
+        var beatmapChanged = ApplyMods(
+            tmpBeatmap, 
+            request.mods.Where(m => m.ToLower() == "da"), 
+            request.da_settings
+        );
+        if (beatmapChanged)
+        {
+            tmpBeatmap.Save();
+        }
+        var tmpBeatmapFilePath = GetTmpBeatmapFilePath(beatmapId);
+        var args = GeneratePerformanceCalculatorArgs(request, tmpBeatmapFilePath);
+        response = GetSimulationJson(args, simulationLock);
+        File.Delete(tmpBeatmapFilePath);
+    }
     Console.WriteLine("Response:");
     Console.WriteLine(response);
     return response;
@@ -82,30 +208,55 @@ app.MapPost("/simulate/dt", async (SimulateDtRequest request) =>
 {
     Console.WriteLine($"POST: /simulate/dt ({request})");
     var beatmapId = request.beatmap_id;
-    var beatmapFilePath = $"cache/{beatmapId}.osu";
-    if (!File.Exists(beatmapFilePath))
-    {
-        var beatmapFile = await httpClient.GetByteArrayAsync($"https://osu.ppy.sh/osu/{beatmapId}");
-        File.WriteAllBytes(beatmapFilePath, beatmapFile);
-    }
+    await DownloadBeatmapIfNeeded(beatmapId, httpClient);
     string response;
     lock (mapEditingLock)
     {
-        var tmpBeatmapFilePath = "tmp.osu";
-        if (File.Exists(tmpBeatmapFilePath))
-        {
-            File.Delete(tmpBeatmapFilePath);
-        }
-        File.Copy(beatmapFilePath, tmpBeatmapFilePath);
-        var beatmap = new Beatmap(tmpBeatmapFilePath);
+        var tmpBeatmap = GetTmpBeatmap(beatmapId);
+        var beatmapChanged = ApplyMods(tmpBeatmap, request.mods, request.da_settings);
         var dtRate = request.dt_rate;
         var newBaseRate = dtRate / 1.5M;
         if (Math.Abs(newBaseRate - 1M) > 0.001M)
         {
-            ChangeBeatmapSpeed(beatmap, newBaseRate);
-            beatmap.Save();
+            ChangeBeatmapSpeed(tmpBeatmap, newBaseRate);
+            beatmapChanged = true;
         }
-        var args = GeneratePerformanceCalculatorArgs(request, tmpBeatmapFilePath);
+        if (beatmapChanged)
+        {
+            tmpBeatmap.Save();
+        }
+        var tmpBeatmapFilePath = GetTmpBeatmapFilePath(beatmapId);
+        var args = GeneratePerformanceCalculatorArgsDTHT(request, tmpBeatmapFilePath);
+        response = GetSimulationJson(args, simulationLock);
+        File.Delete(tmpBeatmapFilePath);
+    }
+    Console.WriteLine("Response:");
+    Console.WriteLine(response);
+    return response;
+});
+app.MapPost("/simulate/ht", async (SimulateHtRequest request) =>
+{
+    Console.WriteLine($"POST: /simulate/ht ({request})");
+    var beatmapId = request.beatmap_id;
+    await DownloadBeatmapIfNeeded(beatmapId, httpClient);
+    string response;
+    lock (mapEditingLock)
+    {
+        var tmpBeatmap = GetTmpBeatmap(beatmapId);
+        var beatmapChanged = ApplyMods(tmpBeatmap, request.mods, request.da_settings);
+        var htRate = request.ht_rate;
+        var newBaseRate = htRate / 0.75M;
+        if (Math.Abs(newBaseRate - 1M) > 0.001M)
+        {
+            ChangeBeatmapSpeed(tmpBeatmap, newBaseRate);
+            beatmapChanged = true;
+        }
+        if (beatmapChanged)
+        {
+            tmpBeatmap.Save();
+        }
+        var tmpBeatmapFilePath = GetTmpBeatmapFilePath(beatmapId);
+        var args = GeneratePerformanceCalculatorArgsDTHT(request, tmpBeatmapFilePath);
         response = GetSimulationJson(args, simulationLock);
         File.Delete(tmpBeatmapFilePath);
     }
@@ -122,11 +273,12 @@ app.Run();
 
 record SimulateRequest(
     string[] mods,
-    int? combo,
     int misses,
     int mehs,
     int goods,
-    int beatmap_id
+    int beatmap_id,
+    int? combo = null,
+    DifficultyAdjustSettings? da_settings = null
 ) {
     public override string ToString()
     {
@@ -137,15 +289,45 @@ record SimulateRequest(
 record SimulateDtRequest(
     decimal dt_rate,
     string[] mods,
-    int? combo,
     int misses,
     int mehs,
     int goods,
-    int beatmap_id
-) : SimulateRequest(mods, combo, misses, mehs, goods, beatmap_id)
+    int beatmap_id,
+    int? combo = null,
+    DifficultyAdjustSettings? da_settings = null
+) : SimulateRequest(mods, misses, mehs, goods, beatmap_id, combo, da_settings)
 {
     public override string ToString()
     {
         return $"SimulateDtRequest {{ dt_rate = {dt_rate}, mods = [{string.Join(", ", mods)}], combo = {combo}, misses = {misses}, mehs = {mehs}, goods = {goods}, beatmap_id = {beatmap_id} }}";
     }
 };
+
+record SimulateHtRequest(
+    decimal ht_rate,
+    string[] mods,
+    int misses,
+    int mehs,
+    int goods,
+    int beatmap_id,
+    int? combo = null,
+    DifficultyAdjustSettings? da_settings = null
+) : SimulateRequest(mods, misses, mehs, goods, beatmap_id, combo, da_settings)
+{
+    public override string ToString()
+    {
+        return $"SimulateHtRequest {{ ht_rate = {ht_rate}, mods = [{string.Join(", ", mods)}], combo = {combo}, misses = {misses}, mehs = {mehs}, goods = {goods}, beatmap_id = {beatmap_id} }}";
+    }
+};
+
+record DifficultyAdjustSettings(
+    decimal ar,
+    decimal cs,
+    decimal hp,
+    decimal od
+) {
+    public override string ToString()
+    {
+        return $"DifficultyAdjustSettings {{ ar = {ar}, cs = {cs}, hp = {hp}, od = {od} }}";
+    }
+}
